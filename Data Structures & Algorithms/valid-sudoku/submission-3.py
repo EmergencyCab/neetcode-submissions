5096eba457806729546906515c79070ff90613aa
Prashant Panta <prashantpanta777@gmail.com>
2026-06-24 10:34:39 -0500

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #Create 3 hassets to store values 
+        #Check all 9 boxes and square for duplicates in the hashset 
+        #If not duplicated update the hassets 
+        #return true 
+
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        square = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                    board [r][c] in cols[c] or
+                    board[r][c] in square[(r//3, c//3)]):
+                        return False 
+            
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                square[(r//3,c//3)].add(board[r][c])
+
+        return True
+
+
+        
